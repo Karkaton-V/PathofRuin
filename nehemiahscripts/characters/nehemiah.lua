4d38f941337a28fd8498eca3bc1a3e7918c45a11
Karkaton-V <125698830+Karkaton-V@users.noreply.github.com>
2025-03-16 16:52:48 -0500

    mass restructuring

---
+++ nehemiahscripts/characters/nehemiah.lua
@@ -0,0 +1,31 @@
+local game = POR.Game()
+
+local NEHEMIAH_TYPE = Isaac.GetPlayerTypeByName("Nehemiah", false)                              -- Nehemiah
+local TAINTED_NEHEMIAH_TYPE = Isaac.GetPlayerTypeByName("The Condemned", true)                  -- Tainted Nehemiah
+local NEHEMIAH_BEARD_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/nehemiah_beard.anm2")   -- Nehemiah's Costume
+local NEHEMIAHSHAMMER_ITEM_ID = Isaac.GetItemIdByName("Nehemiah's Hammer")                      -- Item Id of Nehemiah's Hammer
+local BOOKOFEZRA_ITEM_ID = Isaac.GetItemIdByName("Book of Ezra")                                -- Item Id of Book of Ezra
+
+--- @param player EntityPlayer       -- establishes that player references an EntityPlayer
+function POR:NehemiahInit(player)
+    if player:GetPlayerType() ~= NEHEMIAH_TYPE then
+        return -- If not Nehemiah, exits
+    end
+    player:AddNullCostume(NEHEMIAH_BEARD_COSTUME)
+    -- "KeepinPools?" is bugged, so if set to false, game will always crash when trying to continue a run
+    player:SetPocketActiveItem(NEHEMIAHSHAMMER_ITEM_ID, ActiveSlot.SLOT_POCKET, true)
+
+    local pool = game:GetItemPool()
+    pool:RemoveCollectible(NEHEMIAHSHAMMER_ITEM_ID)
+
+end
+function POR:TaintedNehemiahInit(player)
+   if player:GetPlayerType() ~= TAINTED_NEHEMIAH_TYPE then
+      return -- If not Tainted Nehemiah, exits
+   end
+    player:SetPocketActiveItem(BOOKOFEZRA_ITEM_ID, ActiveSlot.SLOT_POCKET, true)
+
+    local pool = game:GetItemPool()
+    pool:RemoveCollectible(BOOKOFEZRA_ITEM_ID)
+
+end
---
+++ nehemiahscripts/items/book_of_ezra.lua
@@ -0,0 +1,12 @@
+local game = POR.Game()
+
+local BOOKOFEZRA_ITEM_ID = Isaac.GetItemIdByName("Book of Ezra")        -- item id of Book of Ezra
+local MOONLIGHTEFFECT = Isaac.GetEntityTypeByName("Ezra's Moonlight")   -- effect id of the book's light
+
+function POR:BookofEzraUse(_, _, player)
+    local room = game:GetRoom()
+    Isaac.Spawn(EntityType.ENTITY_EFFECT, Isaac.GetEntityVariantByName("Ezra's Moonlight") , 1, room:GetCenterPos(), Vector.Zero, nil) 
+
+
+    return true
+end
