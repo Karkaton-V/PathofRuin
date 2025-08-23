782b8d76004510a8358f6e72b1fcb5e2a1f16bf4
Karkaton-V <125698830+Karkaton-V@users.noreply.github.com>
2025-08-23 16:03:51 -0500

    Whole Buncha Stuff
    Spritesheets, Items, Familiars, UI, Animations and Blueprint

--- nehemiahscripts/characters/nehemiah.lua
+++ nehemiahscripts/characters/nehemiah.lua
@@ -1,8 +1,9 @@
 local game = POR.Game()
 
 local NEHEMIAH_TYPE = Isaac.GetPlayerTypeByName("Nehemiah", false)                              -- Nehemiah
-local TAINTED_NEHEMIAH_TYPE = Isaac.GetPlayerTypeByName("The Condemned", true)                  -- Tainted Nehemiah
-local NEHEMIAH_BEARD_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/nehemiah_beard.anm2")   -- Nehemiah's Costume
+local TAINTED_NEHEMIAH_TYPE = Isaac.GetPlayerTypeByName("The Condemned", true)                  -- T. Nehemiah
+local NEHEMIAH_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/nehemiah_extra.anm2")         -- Nehemiah's Costume
+local NEHEMIAHB_COSTUME = Isaac.GetCostumeIdByPath("gfx/characters/nehemiahb_extra.anm2")        -- T. Nehemiah's Costume
 local NEHEMIAHSHAMMER_ITEM_ID = Isaac.GetItemIdByName("Nehemiah's Hammer")                      -- Item Id of Nehemiah's Hammer
 local BOOKOFEZRA_ITEM_ID = Isaac.GetItemIdByName("Book of Ezra")                                -- Item Id of Book of Ezra
 
@@ -11,7 +12,7 @@ function POR:NehemiahInit(player)
     if player:GetPlayerType() ~= NEHEMIAH_TYPE then
         return -- If not Nehemiah, exits
     end
-    player:AddNullCostume(NEHEMIAH_BEARD_COSTUME)
+    player:AddNullCostume(NEHEMIAH_COSTUME)
     -- "KeepinPools?" is bugged, so if set to false, game will always crash when trying to continue a run
     player:SetPocketActiveItem(NEHEMIAHSHAMMER_ITEM_ID, ActiveSlot.SLOT_POCKET, true)
 
@@ -23,6 +24,7 @@ function POR:TaintedNehemiahInit(player)
    if player:GetPlayerType() ~= TAINTED_NEHEMIAH_TYPE then
       return -- If not Tainted Nehemiah, exits
    end
+   player:AddNullCostume(NEHEMIAHB_COSTUME)
     player:SetPocketActiveItem(BOOKOFEZRA_ITEM_ID, ActiveSlot.SLOT_POCKET, true)
 
     local pool = game:GetItemPool()
